0d00e83b8a26bdcfc553b3d41fad61b0730a5261
WilliamDeckmann <u773560@rts-365.dk>
2021-10-29 10:40:14 +0200

    Update list function

--- assets/dist/js/list.js
+++ assets/dist/js/list.js
@@ -4,14 +4,98 @@
 var list = document.querySelector(".List");
 var button = document.querySelector(".Remove-all"); // Array variables
 
-var itemArray = []; // Loop
+var itemArray = [];
+/*
+// Loop: Swapi
+for(let i = 1; i <= 5; i++) {       
+    let star = `https://swapi.dev/api/people/${i}`;
+    axios.get(star)
+        .then(response => {
+            var data = response.data;
+            itemArray.push(data.name);
+            console.log(data.name);
+            
 
-for (var i = 1; i <= 5; i++) {
-  var star = "https://swapi.dev/api/people/".concat(i);
-  axios.get(star).then(function (response) {
+
+            // Create: item
+            let item = document.createElement("li");
+            list.appendChild(item);
+            item.classList.add("List__item")
+
+            // Create: remove
+            let remove = document.createElement("li");
+            item.appendChild(remove);
+            remove.classList.add("List__remove")
+            remove.innerHTML = `
+                <p class="List__text">
+                    Remove
+                </p>
+            `;
+
+            // Create: save
+            let save = document.createElement("li");
+            item.appendChild(save);
+            save.classList.add("List__save")
+            save.innerHTML = `
+                <p class="List__text">
+                    Save
+                </p>
+            `;
+
+            // Create: title
+            let title = document.createElement("li");
+            item.appendChild(title);
+            title.classList.add("List__title")
+            title.innerHTML = `
+                <p class="List__text">
+                    Name: ${data.name}
+                </p>
+            `;
+            
+            // Swipe left
+            let active = false;
+            title.addEventListener("click", () => {
+                if(active == false) {
+                    title.style.left = "-300px";
+                    active = true;
+                }else{
+                    title.style.left = "0px";
+                    active = false;
+                }
+            });
+
+            // Remove
+            remove.addEventListener("click", () => {
+                item.remove();
+            });
+
+            // Remove all
+            button.addEventListener("click", () => {
+                item.remove();
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {
+            //...
+        });
+};
+*/
+// Joke array
+
+var jokeArray = ["0189hNRf2g", "08EQZ8EQukb", "08xHQCdx5Ed"]; // Loop: icanhazdadjoke
+
+for (var i = 0; i < jokeArray.length; i++) {
+  var ican = "https://icanhazdadjoke.com/j/".concat(jokeArray[i]);
+  axios.get(ican, {
+    headers: {
+      Accept: "application/json"
+    }
+  }).then(function (response) {
     var data = response.data;
-    itemArray.push(data.name);
-    console.log(data.name); // Create: item
+    itemArray.push(data.joke);
+    console.log(data.joke); // Create: item
 
     var item = document.createElement("li");
     list.appendChild(item);
@@ -30,7 +114,7 @@ for (var i = 1; i <= 5; i++) {
     var title = document.createElement("li");
     item.appendChild(title);
     title.classList.add("List__title");
-    title.innerHTML = "\n                <p class=\"List__text\">\n                    Name: ".concat(data.name, "\n                </p>\n            "); // Swipe left
+    title.innerHTML = "\n                <p class=\"List__text\">\n                    ".concat(data.joke, "\n                </p>\n            "); // Swipe left
 
     var active = false;
     title.addEventListener("click", function () {
@@ -70,15 +154,16 @@ for (var _i = 0; _i < itemArray.length; _i++) {
 } // icanhazdadjoke
 
 
-var url = "https://icanhazdadjoke.com"; // `https://icanhazdadjoke.com/j/${ joke id }>`
+var url = "https://icanhazdadjoke.com"; // `https://icanhazdadjoke.com/j/${ joke id }`
 // Axios API
 
-axios.get(url).then(function (response) {
+axios.get(url, {
+  headers: {
+    Accept: "application/json"
+  }
+}).then(function (response) {
   console.log("Axios:");
   console.log(response);
-})["catch"](function (error) {
-  console.log(error);
-}).then(function () {// always executed
 }); // Fetch API
 
 fetch(url, {
--- assets/src/js/list.js
+++ assets/src/js/list.js
@@ -5,7 +5,8 @@ const button = document.querySelector(".Remove-all");
 // Array variables
 let itemArray = [];
 
-// Loop
+/*
+// Loop: Swapi
 for(let i = 1; i <= 5; i++) {       
     let star = `https://swapi.dev/api/people/${i}`;
     axios.get(star)
@@ -80,6 +81,98 @@ for(let i = 1; i <= 5; i++) {
             //...
         });
 };
+*/
+
+
+
+// Joke array
+let jokeArray = [
+    "0189hNRf2g",
+    "08EQZ8EQukb",
+    "08xHQCdx5Ed"
+];
+
+// Loop: icanhazdadjoke
+for(let i = 0; i < jokeArray.length; i++) {       
+    let ican = `https://icanhazdadjoke.com/j/${jokeArray[i]}`;
+    axios.get(ican, {
+        headers: {
+            Accept: "application/json",
+        },
+    })
+        .then(response => {
+            var data = response.data;
+            itemArray.push(data.joke);
+            console.log(data.joke);
+            
+
+
+            // Create: item
+            let item = document.createElement("li");
+            list.appendChild(item);
+            item.classList.add("List__item")
+
+            // Create: remove
+            let remove = document.createElement("li");
+            item.appendChild(remove);
+            remove.classList.add("List__remove")
+            remove.innerHTML = `
+                <p class="List__text">
+                    Remove
+                </p>
+            `;
+
+            // Create: save
+            let save = document.createElement("li");
+            item.appendChild(save);
+            save.classList.add("List__save")
+            save.innerHTML = `
+                <p class="List__text">
+                    Save
+                </p>
+            `;
+
+            // Create: title
+            let title = document.createElement("li");
+            item.appendChild(title);
+            title.classList.add("List__title")
+            title.innerHTML = `
+                <p class="List__text">
+                    ${data.joke}
+                </p>
+            `;
+            
+            // Swipe left
+            let active = false;
+            title.addEventListener("click", () => {
+                if(active == false) {
+                    title.style.left = "-300px";
+                    active = true;
+                }else{
+                    title.style.left = "0px";
+                    active = false;
+                }
+            });
+
+            // Remove
+            remove.addEventListener("click", () => {
+                item.remove();
+            });
+
+            // Remove all
+            button.addEventListener("click", () => {
+                item.remove();
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+        .then(function() {
+            //...
+        });
+};
+
+
 
 
 
@@ -99,19 +192,17 @@ for(let i = 0; i < itemArray.length; i++) {
 
 
 // icanhazdadjoke
-let url = "https://icanhazdadjoke.com"; // `https://icanhazdadjoke.com/j/${ joke id }>`
+let url = "https://icanhazdadjoke.com"; // `https://icanhazdadjoke.com/j/${ joke id }`
 
 // Axios API
-axios.get(url)
+axios.get(url, {
+    headers: {
+        Accept: "application/json",
+    },
+})
     .then(response => {
         console.log("Axios:");
         console.log(response);
-    })
-    .catch(function(error) {
-        console.log(error);
-    })
-    .then(function() {
-        // always executed
     });
 
 // Fetch API
